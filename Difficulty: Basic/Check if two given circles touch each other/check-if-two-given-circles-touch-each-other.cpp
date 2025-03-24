d38062a5b2a07a0a8a81430599ad1e303782d2bd
Kunal <121505871+kunal7bhalla@users.noreply.github.com>
2025-03-24 15:03:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check if two given circles touch each other/check-if-two-given-circles-touch-each-other.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int circleTouch(int X1, int Y1, int R1, int X2, int Y2, int R2) {
+        // code here
+        
+        int d=sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+        
+        if(d<R1+R2){
+            return 1;
+        }
+        
+        if(d==R1+R2){
+            return 1;
+        }
+        
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int X1, Y1, R1, X2, Y2, R2;
+        cin >> X1 >> Y1 >> R1 >> X2 >> Y2 >> R2;
+        Solution ob;
+        cout << ob.circleTouch(X1, Y1, R1, X2, Y2, R2) << "\n";
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends

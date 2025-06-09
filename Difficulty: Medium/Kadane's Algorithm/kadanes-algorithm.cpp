23f2de52c3a1a1e9d85153fb7b930cbf9c1c4def
Kunal <121505871+kunal7bhalla@users.noreply.github.com>
2025-06-09 15:03:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -1,53 +1,21 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int maxSubarraySum(vector<int> &arr) {
         // Your code here
         
-        int sum=0;
-        int maxi=arr[0];
+        int sum=arr[0];
+        int temp=0;
         
         for(int i=0;i<arr.size();i++){
-            sum+=arr[i];
+            temp+=arr[i];
             
-            maxi=max(maxi,sum);
+            sum=max(sum,temp);
             
-            if(sum<0){
-                sum=0;
+            if(temp<0){
+                temp=0;
             }
         }
         
-        return maxi;
+        return sum;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To discard any leftover newline characters
-    while (t--)   // while testcases exist
-    {
-        vector<int> arr;
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        cout << ob.maxSubarraySum(arr) << endl << "~" << endl;
-    }
-}
-// } Driver Code Ends
